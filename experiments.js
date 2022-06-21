692ac573cff2802401d2ec454f125843ad2c60d1
dwhyq <dwhyq@outlook.com>
2022-06-21 15:02:22 +0100

    js task

---
+++ experiments.js
@@ -0,0 +1,5 @@
+function profile(fullName, height, country) {
+    return `Hi! I am ${fullName}, a ${height} tall ${country}. I am pleased to be part of this year cohort.`;}
+
+    console.log(profile("DwhyQ", "5'6ft", "Nigerian",));
+    document.write(profile("DwhyQ", "5'6ft", "Nigerian",))
